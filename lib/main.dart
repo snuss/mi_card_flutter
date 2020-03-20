9c2cbea5aa333375dd99bb765bdba9e9cfbb7a4d
Sebastian Nuss <sebastian.nuss@me.com>
2020-03-20 10:54:08 +0100

    Changed containers to Cards

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,77 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 100.0,
+                  backgroundImage: NetworkImage(
+                      "https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2600&q=80"),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  "Sebastian Nuss",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Sen',
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "Flutter Developer",
+                  style: TextStyle(
+                    color: Colors.teal.shade200,
+                    fontFamily: 'Oswald',
+                    fontSize: 25.0,
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 55.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      size: 30.0,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      '+49 6131 123765',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                Card(
+                  color: Colors.grey.shade50,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 55.0),
+                  child: ListTile(
+                    leading: Icon(Icons.email, size: 30.0, color: Colors.teal),
+                    title: Text(
+                      'sebastian.nuss@me.com',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/* LAYOUT CHALLENGE:
+
+home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
@@ -48,6 +119,4 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
-    );
-  }
-}
+ */
